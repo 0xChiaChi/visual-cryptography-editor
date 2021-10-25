93306370f4a6be631ccfab83e134d3d0379a2e30
Lingzi <52747242+lingzii@users.noreply.github.com>
2021-10-25 22:06:09 +0800

    refactor

--- js/vc-grid.js
+++ js/vc-grid.js
@@ -1,1 +1,16 @@
-// visual-cr
+// visual-cryptography-grid-system
+var cv = window.cv;
+let imgElement = document.getElementById("imageSrc");
+let inputElement = document.getElementById("fileInput");
+inputElement.addEventListener(
+  "change",
+  (e) => {
+    imgElement.src = URL.createObjectURL(e.target.files[0]);
+  },
+  false
+);
+imgElement.onload = function () {
+  let mat = cv.imread(imgElement);
+  cv.imshow("canvasOutput", mat);
+  mat.delete();
+};

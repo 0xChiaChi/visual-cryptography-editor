08e83be40df851ccea88b9c5ee1c0f0251662a49
Lingzi <52747242+lingzii@users.noreply.github.com>
2021-10-26 08:40:01 +0800

    灰階 & 二值化

--- js/vc-grid.js
+++ js/vc-grid.js
@@ -1,7 +1,11 @@
-// visual-cryptography-grid-system
+// ------------------ visual-cryptography-grid-system ------------------ //
+
 var cv = window.cv;
 let imgElement = document.getElementById("imageSrc");
 let inputElement = document.getElementById("fileInput");
+
+// -------------------------------------------------------------------- //
+
 inputElement.addEventListener(
   "change",
   (e) => {
@@ -9,8 +13,23 @@ inputElement.addEventListener(
   },
   false
 );
+
 imgElement.onload = function () {
-  let mat = cv.imread(imgElement);
-  cv.imshow("canvasOutput", mat);
-  mat.delete();
+  main();
 };
+
+// -------------------------------------------------------------------- //
+
+function main() {
+  let src = cv.imread(imgElement);
+  cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY);
+  cv.threshold(src, src, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU);
+  console.log(src.data);
+  cv.imshow("Share1Layer", src);
+  src.delete();
+}
+
+// ---------------------------- Reference ---------------------------- //
+// - https://docs.opencv.org/3.4.3/de/d06/tutorial_js_basic_ops.html
+// - https://docs.opencv.org/3.3.1/dd/d4d/tutorial_js_image_arithmetics.html
+// - https://www.cnblogs.com/bjxqmy/p/12766075.html

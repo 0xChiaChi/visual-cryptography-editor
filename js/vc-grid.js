58c6ddfb3722f5b3b3bf4f48d044948737a5da03
xiaoyao3312 <xiaoyao3312@gmail.com>
2021-10-29 16:05:55 +0800

    .

--- js/vc-grid.js
+++ js/vc-grid.js
@@ -20,13 +20,59 @@ imgElement.onload = function () {
 
 // -------------------------------------------------------------------- //
 
+function buildNoiseLayer(size) {
+  var canvas = document.createElement("canvas");
+  var context = canvas.getContext("2d");
+  var w = (canvas.width = size["width"]);
+  var h = (canvas.height = size["height"]);
+  for (i = 0; i < w; i++) {
+    for (j = 0; j < h; j++) {
+      var num = Math.floor(Math.random() * 255);
+      num = num < 127 ? 0 : 255;
+      context.fillStyle = "rgb(" + num + "," + num + "," + num + ")";
+      context.fillRect(i, j, 1, 1);
+    }
+  }
+  return cv.imread(canvas);
+}
+
 function main() {
   let src = cv.imread(imgElement);
+  var noise = buildNoiseLayer(src.size());
+  var src_not = new cv.Mat();
+  var noise_not = new cv.Mat();
+
   cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY);
-  cv.threshold(src, src, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU);
-  console.log(src.data);
-  cv.imshow("Share1Layer", src);
+  cv.cvtColor(noise, noise, cv.COLOR_RGBA2GRAY);
+  cv.threshold(src, src, 0, 255, cv.THRESH_OTSU);
+
+  cv.bitwise_not(src, src_not);
+  cv.bitwise_not(noise, noise_not);
+
+  // cv.imshow("Share1Layer", src);
+  // cv.imshow("Share1Layer", noise);
+
+  var dst1 = new cv.Mat();
+  var dst2 = new cv.Mat();
+  var dst3 = new cv.Mat();
+
+  cv.bitwise_and(src, noise_not, dst1);
+  cv.bitwise_and(src_not, noise, dst2);
+  cv.bitwise_or(dst1, dst2, dst3);
+
+  var vertic = new cv.Mat();
+  cv.bitwise_or(dst3, noise, vertic);
+  cv.bitwise_not(vertic, vertic);
+  cv.imshow("Share1Layer", dst3);
+  cv.imshow("Share2Layer", noise);
+  cv.imshow("Share3Layer", vertic);
+
   src.delete();
+  noise.delete();
+  src_not.delete();
+  noise_not.delete();
+  dst1.delete();
+  dst2.delete();
 }
 
 // ---------------------------- Reference ---------------------------- //
